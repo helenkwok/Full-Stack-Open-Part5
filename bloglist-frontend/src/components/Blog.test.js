ef95f85bdacaf5feae4fd4bf84dd6f4561623c81
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-09 22:41:33 +0930

    5.14: Blog list tests, step2

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -35,4 +36,37 @@ test('renders content', () => {
 
   const likes = container.querySelector('.likes')
   expect(likes).not.toBeVisible()
+})
+
+test('url and number of likes are shown when the button has been clicked', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Blogger',
+    url: 'http://www.test.com/blog',
+    likes: 5,
+    user: {
+      username: 'tt123',
+      id: '12345abcdefg',
+      name: 'Tester',
+    }
+  }
+
+  const user = {
+    username: 'tester2',
+    id: '12345abcdefg',
+    name: 'Tester2',
+  }
+
+  const { container } =render(<Blog blog={blog} user={user} />)
+  screen.debug(container)
+
+  const userTester = userEvent.setup()
+  const button = screen.getByText('view')
+  await userTester.click(button)
+
+  const url = container.querySelector('.url')
+  expect(url).toBeVisible()
+
+  const likes = container.querySelector('.likes')
+  expect(likes).toBeVisible()
 })
