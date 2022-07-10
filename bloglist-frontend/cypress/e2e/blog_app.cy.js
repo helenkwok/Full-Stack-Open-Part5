fab84649b472afbba2f05e6a9da851c14f46ce2c
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-10 20:03:06 +0930

    5.19: bloglist end to end testing, step3

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -41,4 +41,41 @@ describe('Blog app', function() {
       cy.get('.error').should('have.css', 'border-style', 'solid')
     })
   })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3003/api/testing/reset')
+      const user = {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
+      cy.visit('http://localhost:3000')
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('salainen')
+      cy.contains('login').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('new note').click()
+
+      const newBlog = {
+        title: 'Component testing is done with react-testing-library',
+        author: 'Blogger',
+        url: 'http://www.test.com/blog'
+      }
+
+      cy.get('form').within(() => {
+        cy.get('input[name=title]').type(newBlog.title)
+        cy.get('input[name=author]').type(newBlog.author)
+        cy.get('input[name=url]').type(newBlog.url)
+        cy.contains('create').click()
+      })
+
+      cy.contains('a new blog Component testing is done with react-testing-library by Blogger added')
+      cy.get('.blog').should('contain', newBlog.title)
+      cy.get('.blog').should('contain', newBlog.author)
+    })
+  })
 })
