5e764bdaf6dcefb077ec5c2d2f5f66ca95991a28
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-10 20:30:55 +0930

    5.20: bloglist end to end testing, step4

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,10 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
+    cy.visit('http://localhost:3000')
     cy.contains('log in to application')
     cy.contains('username')
     cy.contains('password')
@@ -13,7 +13,6 @@ describe('Blog app', function() {
 
   describe('Login',function() {
     beforeEach(function() {
-      cy.request('POST', 'http://localhost:3003/api/testing/reset')
       const user = {
         name: 'Matti Luukkainen',
         username: 'mluukkai',
@@ -40,42 +39,56 @@ describe('Blog app', function() {
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
       cy.get('.error').should('have.css', 'border-style', 'solid')
     })
-  })
 
-  describe('When logged in', function() {
-    beforeEach(function() {
-      cy.request('POST', 'http://localhost:3003/api/testing/reset')
-      const user = {
-        name: 'Matti Luukkainen',
-        username: 'mluukkai',
-        password: 'salainen'
-      }
-      cy.request('POST', 'http://localhost:3003/api/users/', user)
-      cy.visit('http://localhost:3000')
-      cy.get('input:first').type('mluukkai')
-      cy.get('input:last').type('salainen')
-      cy.contains('login').click()
-    })
-
-    it('A blog can be created', function() {
-      cy.contains('new note').click()
-
-      const newBlog = {
-        title: 'Component testing is done with react-testing-library',
-        author: 'Blogger',
-        url: 'http://www.test.com/blog'
-      }
-
-      cy.get('form').within(() => {
-        cy.get('input[name=title]').type(newBlog.title)
-        cy.get('input[name=author]').type(newBlog.author)
-        cy.get('input[name=url]').type(newBlog.url)
-        cy.contains('create').click()
+    describe('When logged in', function() {
+      beforeEach(function() {
+        localStorage.clear()
+        cy.request('POST', 'http://localhost:3003/api/login', {
+          username: 'mluukkai', password: 'salainen'
+        }).then(response => {
+          localStorage.setItem('loggedBloglistUser', JSON.stringify(response.body))
+          cy.visit('http://localhost:3000')
+        })
       })
 
-      cy.contains('a new blog Component testing is done with react-testing-library by Blogger added')
-      cy.get('.blog').should('contain', newBlog.title)
-      cy.get('.blog').should('contain', newBlog.author)
+      it('A blog can be created', function() {
+        cy.contains('new note').click()
+
+        const newBlog = {
+          title: 'Component testing is done with react-testing-library',
+          author: 'Blogger',
+          url: 'http://www.test.com/blog'
+        }
+
+        cy.get('form').within(() => {
+          cy.get('input[name=title]').type(newBlog.title)
+          cy.get('input[name=author]').type(newBlog.author)
+          cy.get('input[name=url]').type(newBlog.url)
+          cy.contains('create').click()
+        })
+
+        cy.contains('a new blog Component testing is done with react-testing-library by Blogger added')
+        cy.get('.blog').should('contain', newBlog.title)
+        cy.get('.blog').should('contain', newBlog.author)
+      })
+
+      describe('and a blog exists', function() {
+        beforeEach(function() {
+          cy.createBlog({
+            title: 'End to end testing is done with Cypress',
+            author: 'Blogger',
+            url: 'http://www.test.com/blog'
+          })
+        })
+
+        it('User can like a blog', function() {
+          cy.get('.blog').within(() => {
+          cy.contains('view').click()
+          cy.get('.likeButton').click()
+          cy.get('.likes').should('contain', 'likes 1')
+          })
+        })
+      })
     })
   })
 })
